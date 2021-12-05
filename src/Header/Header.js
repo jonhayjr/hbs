b8769306ccf47c68fb9c9ab3a5d81f153d24c5b5
jonhayjr <jonhayjr@gmail.com>
2021-12-04 20:51:40 -0800

    Initialized repo

---
+++ src/Header/Header.js
@@ -0,0 +1,11 @@
+import './Header.css';
+
+const Header = () => {
+    return (
+        <header>
+            <h1>Sherry Anecia Hay</h1>
+        </header>
+    )
+}
+
+export default Header
---
+++ src/Main/Main.js
@@ -0,0 +1,15 @@
+import './Main.css';
+
+const Main = () => {
+    return (
+        <main>
+            <div class="pyro">
+                <div class="before"></div>
+                <p class="text-box">Happy Spirthday Baby!</p>
+                <div class="after"></div>
+            </div>
+        </main>
+    )
+}
+
+export default Main
